02a8657401390f6d55544f956dba4e26e45d5d75
Kaustubh Hatkar <85245568+kaustubhhatkar@users.noreply.github.com>
2022-11-13 14:04:31 +0530

    removed connection link

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://<admin>:<password>@cluster0.osnxala.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("Connection link", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
